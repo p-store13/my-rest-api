e7617544855e0fbb417c1d3739a273bc99f835c4
P-STORE <114459299+p-store13@users.noreply.github.com>
2023-08-09 10:40:49 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
-global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.creator = 'HendraXD'// yourname
+global.MONGO_DB_URI = "mongodb+srv://hendraxd:UpB3gAOvubXajz7G@cluster0.ds5fuj7.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
+global.ACTIVATION_TOKEN_SECRET = "Hendra Ganteng //isi apa aja bebas
+global.your_email = "hgindonesia3@gmail.com" //email
+global.email_password = "ufbdqtihvtitilqa" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
